800c9d01802ec3ca265c2bc802b4f86176900534
Manuel Reeb <51723126+MCoder-coder@users.noreply.github.com>
2024-03-10 10:24:21 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mcoder-coder.github.io/distcv/',
+  base: 'https://github.com/MCoder-coder/distcv',
   integrations: [mdx(), sitemap(), tailwind()]
 });
