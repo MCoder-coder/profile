1216e944fe8deaa8c4bb7a5799e325c772d72b2c
Manuel Reeb <51723126+MCoder-coder@users.noreply.github.com>
2024-03-10 14:43:12 -0300

    otro intento

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/404_BAmmk4xB.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/404_aS0Ufawz.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/cv_D3ZjRJcQ.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/cv_f89miv1Q.mjs');
-
-export { page };
